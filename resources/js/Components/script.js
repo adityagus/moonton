ee1b0126ae982467389c0cac05c670803e975c90
aditya gustian <adityagustian11@gmail.com>
2024-02-13 19:56:43 +0700

    feat: slicing sign in and sign up

---
+++ resources/js/Components/script.js
@@ -0,0 +1,5 @@
+const data = 12
+const dutu = 12
+
+result = data + dutu
+console.log(result)
--- routes/web.php
+++ routes/web.php
@@ -16,22 +16,25 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('admin', function() {
-  return 'Hi Admin';
-})->middleware('role:admin');
+Route::redirect('/', '/prototype/login');
 
-Route::get('user', function() {
-    return 'Hi User';
-})->middleware('role:user');
-
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+Route::prefix('prototype')->name("prototype.")->group(function () {
+    Route::get('/login', function (){
+        return Inertia::render('Prototype/Login');
+    })->name('login');
+    Route::get('/register', function (){
+        return Inertia::render('Prototype/Register');
+    })->name('register');
 });
+
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
